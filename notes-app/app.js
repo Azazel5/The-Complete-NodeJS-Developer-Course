31709f8d3f7f5a967b9d253ef2f8bbfcff3456cb
Azazel5 <subhanga2013@gmail.com>
2022-05-18 13:30:31 -0500

    Command line arguments using yargs
    - Remember to always use 3rd party packages, when it is not the core functionality of the application you're
    trying to make

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,49 @@
 // NPM packages
 const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
 
 // Custom packages
 const getNotes = require('./notes')
 
-console.log(chalk.red.bold.inverse("Success!"))
+/** The process global variable has access to command line arguments. The first two
+ * arguments are path related mumbo jumbo (path to NodeJS and the main file) */
+
+/** Yargs for command line parsing. The version number can be customized */
+yargs.version('999')
+
+yargs.command({
+    command: "add",
+    "describe": "Add a new note",
+    handler: function () {
+        console.log("Adding a new note!")
+    }
+})
+
+yargs.command({
+    command: "remove",
+    "describe": "Remove a note",
+    handler: function () {
+        console.log("Removing a new note!")
+    }
+})
+
+yargs.command({
+    command: "list",
+    "describe": "List all notes",
+    handler: function () {
+        console.log("All notes")
+    }
+})
+
+yargs.command({
+    command: "read",
+    "describe": "Read a note",
+    handler: function () {
+        console.log("Reading a note")
+    }
+})
+
+console.log(yargs.argv)
+
+

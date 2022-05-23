552737a360e05867f9e510b98f54bed2083ce2cc
Azazel5 <subhanga2013@gmail.com>
2022-05-23 15:32:45 -0500

    Refactored the application to use callbacks
    - Saw the power of callbacks: they allow for greater flexibility
    - return console.log() works in a single line; no need for a log and a return on a new line like you used to do

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,41 +1,27 @@
 // Custom Packages
 require('dotenv').config()
+const geoCode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// 3rd party packages
-const request = require('request')
+// App code
+const location = process.argv[2]
 
-const weatherUrl = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=29.4254,-98.4946&units=f`
+if (!location) {
+    return console.log("No location provided!")
+}
 
-request({ url: weatherUrl, json: true }, (error, response) => {
-    if (error) {
-        console.log("Unable to connect to weather service!")
+geoCode(location, (geoCodeError, geoCodeData) => {
+    if (geoCodeError) {
+        return console.log(geoCodeError)
     }
 
-    else if (response.body.error) {
-        console.log("Unable to find location")
-    }
+    forecast(geoCodeData.latitude, geoCodeData.longitude, (forecastError, forecastData) => {
+        if (forecastError) {
+            return console.log(forecastError)
+        }
 
-    else {
-        const currentWeather = response.body.current
-        console.log(`${currentWeather.weather_descriptions[0]}. It is currently ${currentWeather.temperature} degrees out. There is ${currentWeather.precip}% chance of rain.`)
-    }
-})
+        console.log(geoCodeData.location)
+        console.log(forecastData)
+    })
 
-// Geocoding
-const searchText = encodeURI("San Antonio")
-const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?access_token=${process.env.GEOCODING_API_KEY}&limit=1`
-
-request({ url: geoUrl, json: true }, (error, response) => {
-    if (error) {
-        console.log("Unable to connect to geocoding service!")
-    }
-
-    else if (response.body.features.length === 0) {
-        console.log("No matching results found!")
-    }
-
-    else {
-        const geographicalCenter = response.body.features[0].center
-        console.log("Latitude:", geographicalCenter[1], "Longitude:", geographicalCenter[0])
-    }
 })
--- weather-app/playground.js
+++ weather-app/playground.js
@@ -37,16 +37,16 @@
 // 3. After 2 seconds are up, call the callback function with the sum
 // 4. Test your work!
 
-const add = (num1, num2, callback) => {
-    setTimeout(() => {
-        const sum = num1 + num2
-        callback(sum)
-    }, 2000)
-}
+// const add = (num1, num2, callback) => {
+//     setTimeout(() => {
+//         const sum = num1 + num2
+//         callback(sum)
+//     }, 2000)
+// }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
-})
+// add(1, 4, (sum) => {
+//     console.log(sum) // Should print: 5
+// })
 
 /** Notes on asynchronous Node.js */
 

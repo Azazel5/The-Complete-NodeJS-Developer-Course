6c02e8b06931f8c77dbce96413cea3590037faf4
Azazel5 <subhanga2013@gmail.com>
2022-05-23 13:17:13 -0500

    Added error handling to weather app
    - Also explored the callback pattern for async code

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,15 +7,35 @@ const request = require('request')
 const weatherUrl = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=29.4254,-98.4946&units=f`
 
 request({ url: weatherUrl, json: true }, (error, response) => {
-    const currentWeather = response.body.current
-    console.log(`${currentWeather.weather_descriptions[0]}. It is currently ${currentWeather.temperature} degrees out. There is ${currentWeather.precip}% chance of rain.`)
+    if (error) {
+        console.log("Unable to connect to weather service!")
+    }
+
+    else if (response.body.error) {
+        console.log("Unable to find location")
+    }
+
+    else {
+        const currentWeather = response.body.current
+        console.log(`${currentWeather.weather_descriptions[0]}. It is currently ${currentWeather.temperature} degrees out. There is ${currentWeather.precip}% chance of rain.`)
+    }
 })
 
 // Geocoding
-const searchText = encodeURI("Los Angeles")
+const searchText = encodeURI("San Antonio")
 const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?access_token=${process.env.GEOCODING_API_KEY}&limit=1`
 
 request({ url: geoUrl, json: true }, (error, response) => {
-    const geographicalCenter = response.body.features[0].center
-    console.log("Latitude:", geographicalCenter[1], "Longitude:", geographicalCenter[0])
+    if (error) {
+        console.log("Unable to connect to geocoding service!")
+    }
+
+    else if (response.body.features.length === 0) {
+        console.log("No matching results found!")
+    }
+
+    else {
+        const geographicalCenter = response.body.features[0].center
+        console.log("Latitude:", geographicalCenter[1], "Longitude:", geographicalCenter[0])
+    }
 })
--- weather-app/playground.js
+++ weather-app/playground.js
@@ -10,6 +10,44 @@
 
 // console.log('Stopping')
 
+// Callbacks
+// setTimeout(() => {
+//     console.log("Two seconds are up")
+// }, 2000)
+
+// const names = ['Jen', 'Jess', 'Subhanga']
+// const shortNames = names.filter(name => name.length <= 4)
+
+// const geoCode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = { latitude: 123, longitude: 321 }
+//         callback(data)
+//     }, 2000)
+// }
+
+// geoCode('', (data) => {
+//     console.log("Data is available here: ", data)
+// })
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+const add = (num1, num2, callback) => {
+    setTimeout(() => {
+        const sum = num1 + num2
+        callback(sum)
+    }, 2000)
+}
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
+
 /** Notes on asynchronous Node.js */
 
 // If you have a setTimeout with 0 seconds, that is going to run after any synchronous calls like
@@ -17,3 +55,11 @@
 // Watch video #30 (Call stack, queue...) once again, after finishing building out the weather 
 // application!
 // No asynchronous callback gets run until the main function is done running...
+
+// Why use callbacks? The problem with the normal functional approach (perform stuff in a function,
+// return it, and then use it) is that when you're making asynchronous requests, they end up
+// taking some time, so the data isn't returned, so the return value is undefined. When doing
+// time intensive operations, providing callback functions which run only after the data is 
+// successfully received is a useful pattern.
+
+// TLDR: synchronous - return pattern works; asynchronous - callback pattern

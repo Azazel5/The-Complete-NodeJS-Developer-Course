214638be09120fc508834f812f5bd046f2656054
Subhanga Upadhyay <subhanga.upadhyay@concordusa.com>
2022-06-01 12:09:01 -0500

    Using handlebars to create templates for Express
    - Similar to Django's context passing machanism
    - Uses {{}}'s to access the values passed from the Node side

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,15 +5,33 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
 
 // Rendering HTML/JSON/strings using the res.send is all A-ok
 // Pass in an object/array 
+// While using Handlebar, we use render to render a template
+
+// The render function gets an additional argument which is similar to Django's context
+// passing mechanism i.e. pass information from the Node side to the templates!
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Subhanga Upadhyay'
+    })
+})
+
 app.get('/help', (req, res) => {
-    res.send([
-        { help: 'HAHA' },
-        { help: 'SO EZ!!' }
-    ])
+    res.render('help', {
+        helpText: 'Help! I need somebody!'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Subhanga Upadhyay'
+    })
 })
 
 app.get('/about', (req, res) => {

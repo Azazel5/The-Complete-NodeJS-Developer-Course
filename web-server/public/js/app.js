f04d3cf88081a1bc784ab54fd17e0f1c4e89844b
Azazel5 <subhanga2013@gmail.com>
2022-08-10 05:40:15 -0500

    Finished the weather application
    - Wrote client side JS to make a fetch request to the server
    - Displayed response on client
    - Styled the index.hbs file a bit

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,29 @@
-console.log("Loading JS file")
+function makeRequest(e) {
+    e.preventDefault()
+
+    const location = document.querySelector('input').value
+    const p1 = document.querySelector('p.p1')
+    const p2 = document.querySelector('p.p2')
+
+    p1.textContent = 'Loading...'
+    p2.textContent = ''
+
+    fetch(`http://localhost:3000/weather?address=${location}`)
+        .then(response => response.json())
+        .then(data => {
+            const { error, location, forecast } = data
+
+            if (error) {
+                p1.textContent = error
+            }
+
+            else {
+                p1.textContent = location
+                p2.textContent = forecast
+            }
+
+        })
+}
+
+const weatherForm = document.querySelector('form')
+weatherForm.addEventListener('submit', makeRequest)
